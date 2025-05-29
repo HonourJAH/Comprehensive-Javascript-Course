132c96b01e014c8aba131ae6a3257cfc08610bb2
HonourJAH <onojahmicheal@gmail.com>
2025-05-30 00:54:31 +0100

    solved challenge #1, without external help

--- functions/script.js
+++ functions/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 ////////////////////////////////////////
 // Default parameters
@@ -7,7 +7,7 @@
 const bookings = [];
 
 const createBooking = function (
-  flightNum = "LH112",
+  flightNum = 'LH112',
   numPassengers = 4,
   price = 134
 ) {
@@ -33,20 +33,20 @@ const createBooking = function (
 // How Passing Arguments Works: Value vs. Reference
 ///////////////////////////////////////////////////////////////
 
-const flight = "LH234";
+const flight = 'LH234';
 const mike = {
-  name: "Michael Mike",
+  name: 'Michael Mike',
   passport: 23456789,
 };
 
 const checkIn = function (flightNum, passenger) {
-  flightNum = "LH889";
-  passenger.name = "Mr. " + passenger.name;
+  flightNum = 'LH889';
+  passenger.name = 'Mr. ' + passenger.name;
 
   if (passenger.passport === 23456789) {
-    alert("Checked in");
+    alert('Checked in');
   } else {
-    alert("Wrong passport!");
+    alert('Wrong passport!');
   }
 };
 
@@ -66,12 +66,12 @@ const newPassport = function (person) {
 /////////////////////////////////////////////
 
 const oneWord = function (str) {
-  return str.replace(/ /g, "").toLowerCase();
+  return str.replace(/ /g, '').toLowerCase();
 };
 
 const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(" ");
-  return [first.toUpperCase(), ...others].join(" ");
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
 };
 
 // Higer-order function
@@ -94,12 +94,12 @@ const greet = function (greeting) {
   };
 };
 
-const greetHey = greet("Hey");
+const greetHey = greet('Hey');
 // greetHey("Mike");
 
 // greet("Hello")("Mike");
 
-const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 // greetArr("Hi")("Michael");
 
@@ -108,8 +108,8 @@ const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
 /////////////////////////////////////
 
 const lufthansa = {
-  airline: "Lufthansa",
-  iataCode: "LH",
+  airline: 'Lufthansa',
+  iataCode: 'LH',
   bookings: [],
   // book: function() {}
   book(flightNum, name) {
@@ -124,8 +124,8 @@ const lufthansa = {
 // lufthansa.book(635, "Michael");
 
 const eurowings = {
-  airline: "Eurowings",
-  iataCode: "EW",
+  airline: 'Eurowings',
+  iataCode: 'EW',
   bookings: [],
 };
 
@@ -135,26 +135,26 @@ const book = lufthansa.book;
 // book(23, 'Sarah Williams');
 
 // Call method
-book.call(eurowings, 23, "Sarah Williams");
+// book.call(eurowings, 23, 'Sarah Williams');
 // console.log(eurowings);
 
-book.call(lufthansa, 239, "Mary Cooper");
+// book.call(lufthansa, 239, 'Mary Cooper');
 // console.log(lufthansa);
 
 const swiss = {
-  airline: "Swiss Air Lines",
-  iataCode: "LX",
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
   bookings: [],
 };
 
-book.call(swiss, 583, "Mary Cooper");
+// book.call(swiss, 583, 'Mary Cooper');
 
 // Apply method
-const flightData = [583, "George Cooper"];
-book.apply(swiss, flightData);
-console.log(swiss);
+const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
 
-book.call(swiss, ...flightData);
+// book.call(swiss, ...flightData);
 
 ///////////////////////////////////////
 // The bind Method
@@ -166,11 +166,11 @@ const bookEW = book.bind(eurowings);
 const bookLH = book.bind(lufthansa);
 const bookLX = book.bind(swiss);
 
-bookEW(23, "Steven Williams");
+// bookEW(23, 'Steven Williams');
 
 const bookEW23 = book.bind(eurowings, 23);
-bookEW23("Jonas Schmedtmann");
-bookEW23("Martha Cooper");
+// bookEW23('Jonas Schmedtmann');
+// bookEW23('Martha Cooper');
 
 // With Event Listeners
 lufthansa.planes = 300;
@@ -183,18 +183,18 @@ lufthansa.buyPlane = function () {
 // lufthansa.buyPlane();
 
 document
-  .querySelector(".buy")
-  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
 // Partial application
 const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+// console.log(addTax(0.1, 200));
 
 const addVAT = addTax.bind(null, 0.23);
 // addVAT = value => value + value * 0.23;
 
-console.log(addVAT(100));
-console.log(addVAT(23));
+// console.log(addVAT(100));
+// console.log(addVAT(23));
 
 const addTaxRate = function (rate) {
   return function (value) {
@@ -202,5 +202,77 @@ const addTaxRate = function (rate) {
   };
 };
 const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
-console.log(addVAT2(23));
+// console.log(addVAT2(100));
+// console.log(addVAT2(23));
+
+///////////////////////////////////////
+// Coding Challenge #1
+////////////////////////////////////
+
+// Let's build a simple poll app!
+
+// A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+// Here are your tasks:
+
+// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+//   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+//         What is your favourite programming language?
+//         0: JavaScript
+//         1: Python
+//         2: Rust
+//         3: C++
+//         (Write option number)
+//   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+// 2. Call this method whenever the user clicks the "Answer poll" button.
+// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+// 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+// HINT: Use many of the tools you learned about in this and the last section 😉
+
+// BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+// BONUS TEST DATA 1: [5, 2, 3]
+// BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+// GOOD LUCK 😀
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+};
+
+poll.registerNewAnswer = function () {
+  const option = Number(
+    prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    )
+  );
+
+  option >= 0 && option < this.answers.length
+    ? this.answers[option]++
+    : alert('Incorrect option number');
+
+  const result = prompt(
+    'Enter how you would like the result displayed.\nArray or String?: '
+  );
+  this.displayResults(result);
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults = function (type = 'array') {
+  const ans = type.toLowerCase();
+
+  ans === 'string'
+    ? console.log(`Poll results are ${this.answers.join(', ')}`)
+    : console.log(this.answers);
+};
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
