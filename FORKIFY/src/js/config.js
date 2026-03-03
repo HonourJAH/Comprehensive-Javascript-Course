49da6313e294081557c43cf714456c660e6264b0
HonourJAH <onojahmicheal@gmail.com>
2026-03-03 02:55:58 +0100

    commit forkify app

---
+++ FORKIFY/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "0ceb0807-23be-4184-9a23-1f64fdd9b523";
+export const MODAL_CLOSE_SEC = 2.5;
